7779db6ec2512f19d428dbc6dbd0ff4ba035f017
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-24 17:27:56 +0300

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,43 +2,41 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-refs = {
-  startBtn: document.querySelector('[data-start]'),
-  stopBtn: document.querySelector('[data-stop]'),
-  body: document.querySelector('body'),
-};
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
 let timerId = null;
 
-refs.startBtn.addEventListener('click', addEventStart);
+startBtn.addEventListener('click', addEventStart);
 
 function addEventStart() {
-  refs.startBtn.setAttribute('disabled', 'disabled');
-  refs.stopBtn.removeAttribute('disabled', 'disabled');
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled', 'disabled');
   timerId = setInterval(() => {
-    refs.body.style.backgroundColor = getRandomHexColor();
+    body.style.backgroundColor = getRandomHexColor();
   }, 1000);
 }
 
-refs.stopBtn.addEventListener('click', addEventStop);
+stopBtn.addEventListener('click', addEventStop);
 
 function addEventStop() {
-  refs.startBtn.removeAttribute('disabled', 'disabled');
-  refs.stopBtn.setAttribute('disabled', 'disabled');
+  startBtn.removeAttribute('disabled', 'disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
   clearInterval(timerId);
 }
 
-refs.startBtn.style.borderRadius = `${5}px`;
-refs.stopBtn.style.borderRadius = `${5}px`;
-refs.startBtn.style.borderColor = `transparent`;
-refs.stopBtn.style.borderColor = `transparent`;
-refs.startBtn.style.padding = `${10}px ${20}px`;
-refs.stopBtn.style.padding = `${10}px ${20}px`;
-refs.startBtn.style.display = 'flex';
-refs.stopBtn.style.display = 'flex';
-refs.startBtn.style.margin = `${0}px auto ${10}px ${650}px`;
-refs.stopBtn.style.margin = `${0}px auto`;
-refs.startBtn.style.fontFamily = `montserrat`;
-refs.startBtn.style.fontSize = `${20}px`;
-refs.stopBtn.style.fontFamily = `montserrat`;
-refs.stopBtn.style.fontSize = `${20}px`;
+startBtn.style.borderRadius = `${5}px`;
+stopBtn.style.borderRadius = `${5}px`;
+startBtn.style.borderColor = `transparent`;
+stopBtn.style.borderColor = `transparent`;
+startBtn.style.padding = `${10}px ${20}px`;
+stopBtn.style.padding = `${10}px ${20}px`;
+startBtn.style.display = 'flex';
+stopBtn.style.display = 'flex';
+startBtn.style.margin = `${0}px auto ${10}px ${650}px`;
+stopBtn.style.margin = `${0}px auto`;
+startBtn.style.fontFamily = `montserrat`;
+startBtn.style.fontSize = `${20}px`;
+stopBtn.style.fontFamily = `montserrat`;
+stopBtn.style.fontSize = `${20}px`;
