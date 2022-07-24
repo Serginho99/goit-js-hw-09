5987ead1ba24c509fdc7ac45d48663750fdf6a5d
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-24 16:34:20 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+let timerId = null;
+
+refs.startBtn.addEventListener('click', addEventStart);
+
+function addEventStart() {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  refs.stopBtn.removeAttribute('disabled', 'disabled');
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+refs.stopBtn.addEventListener('click', addEventStop);
+
+function addEventStop() {
+  refs.startBtn.removeAttribute('disabled', 'disabled');
+  refs.stopBtn.setAttribute('disabled', 'disabled');
+  clearInterval(timerId);
+}
+
+refs.startBtn.style.borderRadius = `${5}px`;
+refs.stopBtn.style.borderRadius = `${5}px`;
+refs.startBtn.style.borderColor = `transparent`;
+refs.stopBtn.style.borderColor = `transparent`;
+refs.startBtn.style.padding = `${15}px ${25}px`;
+refs.stopBtn.style.padding = `${15}px ${25}px`;
+refs.startBtn.style.display = 'flex';
+refs.stopBtn.style.display = 'flex';
+refs.startBtn.style.margin = `${0}px auto ${10}px`;
+refs.stopBtn.style.margin = `${0}px auto`;
