461b5c6d804c896163a0316847d13788852a0a4d
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-24 18:54:54 +0300

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,9 +1,13 @@
+import '/src/css/common.css';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 const startBtn = document.querySelector('[data-start]');
+startBtn.classList.add('btn');
 const stopBtn = document.querySelector('[data-stop]');
+stopBtn.classList.add('btn');
 const body = document.querySelector('body');
 
 let timerId = null;
@@ -25,18 +29,3 @@ function addEventStop() {
   stopBtn.setAttribute('disabled', 'disabled');
   clearInterval(timerId);
 }
-
-startBtn.style.borderRadius = `${5}px`;
-stopBtn.style.borderRadius = `${5}px`;
-startBtn.style.borderColor = `transparent`;
-stopBtn.style.borderColor = `transparent`;
-startBtn.style.padding = `${10}px ${20}px`;
-stopBtn.style.padding = `${10}px ${20}px`;
-startBtn.style.display = 'flex';
-stopBtn.style.display = 'flex';
-startBtn.style.margin = `${0}px auto ${10}px ${650}px`;
-stopBtn.style.margin = `${0}px auto`;
-startBtn.style.fontFamily = `montserrat`;
-startBtn.style.fontSize = `${20}px`;
-stopBtn.style.fontFamily = `montserrat`;
-stopBtn.style.fontSize = `${20}px`;
